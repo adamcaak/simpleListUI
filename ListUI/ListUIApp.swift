bf816db3ff943db304183de3f4422478d4f9c8c0
adamcaak <ladamczak1997@gmail.com>
2025-01-08 23:25:42 +0100

    Initial Commit

---
+++ ListUI/ListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListUIApp.swift
+//  ListUI
+//
+//  Created by Łukasz Adamczak on 08/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
